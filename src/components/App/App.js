3764af153522d181c52c73b2df475c5a79e9cef2
pedrumond <pe.drumond97@gmail.com>
2024-10-23 21:39:34 -0300

    feat: create and style the Footer component

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,4 +1,5 @@
 import Header from '../Header/Header';
+import Footer from '../Footer/Footer';
 
 function App() {
   return (
@@ -6,6 +7,7 @@ function App() {
       <div className='body'>
         <div className='page'>
           <Header />
+          <Footer />
         </div>
       </div>
     </>
---
+++ src/components/Footer/Footer.js
@@ -0,0 +1,9 @@
+function Footer() {
+  return (
+    <footer class='footer'>
+      <p class='footer__copyright'>&copy; 2024 Pedro Drumond</p>
+    </footer>
+  );
+}
+
+export default Footer;
