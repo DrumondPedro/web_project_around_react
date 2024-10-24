370aff5ed8c73df15b6adcb64a4dbda143f72f86
pedrumond <pe.drumond97@gmail.com>
2024-10-23 22:25:26 -0300

    create and style the PopupWithForm component

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,6 +1,7 @@
 import Header from '../Header/Header';
 import Footer from '../Footer/Footer';
 import Main from '../Main/Main';
+import PopupWithForm from '../PopupWithForm/PopupWithForm';
 
 function App() {
   return (
@@ -17,6 +18,74 @@ function App() {
           </Main>
           <Footer />
         </div>
+        <PopupWithForm title={`Editar perfil`} name={`profile`}>
+          <fieldset className='form__set'>
+            <label className='form__field'>
+              <input
+                type='text'
+                className='form__input form__input_name'
+                id='person'
+                placeholder='Nome'
+                required
+                minength='2'
+                maxength='40'
+              />
+              <span className='form__error person-error'></span>
+            </label>
+            <label className='form__field'>
+              <input
+                type='text'
+                className='form__input form__input_about'
+                id='about'
+                placeholder='Sobre mim'
+                required
+                minength='2'
+                maxength='200'
+              />
+              <span className='form__error about-error'></span>
+            </label>
+          </fieldset>
+        </PopupWithForm>
+        <PopupWithForm title={`Novo local`} name={`gallery`}>
+          <fieldset className='form__set'>
+            <label className='form__field'>
+              <input
+                type='text'
+                className='form__input form__input_title'
+                id='name'
+                placeholder='Título'
+                required
+                minLength='2'
+                maxLength='30'
+              />
+              <span className='form__error name-error'></span>
+            </label>
+            <label className='form__field'>
+              <input
+                type='url'
+                className='form__input form__input_link'
+                id='link'
+                placeholder='Link de imagem'
+                required
+              />
+              <span className='form__error link-error'></span>
+            </label>
+          </fieldset>
+        </PopupWithForm>
+        <PopupWithForm title={`Alterar a foto do perfil`} name={`picture`}>
+          <fieldset className='form__set'>
+            <label className='form__field'>
+              <input
+                type='url'
+                className='form__input form__input_picture'
+                id='picture'
+                placeholder='Link da foto'
+                required
+              />
+              <span className='form__error picture-error'></span>
+            </label>
+          </fieldset>
+        </PopupWithForm>
       </div>
     </>
   );
---
+++ src/components/PopupWithForm/PopupWithForm.js
@@ -0,0 +1,26 @@
+import closeIcon from '../../images/editor/editor_close_Icon.svg';
+
+function PopupWithForm({ children, title, name }) {
+  return (
+    <div className={`editor editor_${name}`}>
+      <div className='editor__content'>
+        <button className={`editor__close-button editor__${name}-close-button`}>
+          <img
+            src={closeIcon}
+            alt='Ícone de um x'
+            className='editor__close-image'
+          />
+        </button>
+        <h2 className='editor__title'>{title}</h2>
+        <form className={`form form_${name} editor__form`} noValidate>
+          {children}
+          <button type='submit' className='form__submit-button'>
+            Salvar
+          </button>
+        </form>
+      </div>
+    </div>
+  );
+}
+
+export default PopupWithForm;
