f3e3f1002075aabdfe721052a70b8c03870643bd
pedrumond <pe.drumond97@gmail.com>
2025-03-21 19:38:26 -0300

    feat: implement Signin validation

--- src/components/Signin/Signin.jsx
+++ src/components/Signin/Signin.jsx
@@ -1,10 +1,63 @@
-import { useState } from 'react';
+import { useContext, useRef, useState } from 'react';
+
+import { LoadingContext } from '../../contexts/LoadingContext';
 
 function Signin() {
+  const { isLoading, setIsLoading } = useContext(LoadingContext);
+
+  const emailRef = useRef();
+  const passwordRef = useRef();
+
+  const [signinData, setSigninData] = useState({ email: '', password: '' });
+
+  const [isValid, setIsValid] = useState({
+    validEmail: ' ',
+    validPassword: ' ',
+  });
+  const [errorMessage, setErrorMessage] = useState({
+    emailMsg: '',
+    passwordMsg: '',
+  });
+  const [isActive, setIsActive] = useState(false);
+
+  const handleEmailChange = (evt) => {
+    setSigninData({ ...signinData, email: evt.target.value });
+    setIsValid({
+      ...isValid,
+      validEmail: emailRef.current.validity.valid,
+    });
+    setErrorMessage({
+      ...errorMessage,
+      emailMsg: emailRef.current.validationMessage,
+    });
+    setIsActive(
+      emailRef.current.validity.valid && emailRef.current.validity.valid
+    );
+  };
+
+  const handlePasswordChange = (evt) => {
+    setSigninData({ ...signinData, password: evt.target.value });
+    setIsValid({
+      ...isValid,
+      validPassword: passwordRef.current.validity.valid,
+    });
+    setErrorMessage({
+      ...errorMessage,
+      passwordMsg: passwordRef.current.validationMessage,
+    });
+    setIsActive(
+      emailRef.current.validity.valid && emailRef.current.validity.valid
+    );
+  };
+
+  async function handleSubimit(evt) {
+    evt.preventDefault();
+  }
+
   return (
     <section className='signin'>
       <h2 className='signin__title'>Entrar</h2>
-      <form className='form signin__form'>
+      <form className='form signin__form' onSubmit={handleSubimit}>
         <fieldset className='form__set signin__form-set'>
           <label className='form__field signin__form-field'>
             <input
@@ -12,9 +65,18 @@ function Signin() {
               type='email'
               id='email'
               placeholder='E-mail'
+              value={signinData.email}
+              onChange={handleEmailChange}
+              ref={emailRef}
               required
             ></input>
-            <span className='form__error'></span>
+            <span
+              className={`form__error form__signin-error ${
+                isValid.validEmail ? `` : `form__error_visible`
+              }`}
+            >
+              {errorMessage.emailMsg}
+            </span>
           </label>
           <label className='form__field'>
             <input
@@ -23,14 +85,27 @@ function Signin() {
               id='password'
               placeholder='Senha'
               minLength='8'
+              value={signinData.password}
+              onChange={handlePasswordChange}
+              ref={passwordRef}
               required
             ></input>
-            <span className='form__error'></span>
+            <span
+              className={`form__error form__signin-error ${
+                isValid.validPassword ? `` : `form__error_visible`
+              }`}
+            >
+              {errorMessage.passwordMsg}
+            </span>
           </label>
         </fieldset>
         <button
-          className='form__submit-button form__signin-submit-button'
           type='submit'
+          disabled={isLoading || !isActive}
+          className={`form__submit-button form__signin-submit-button
+            ${isActive ? `` : `form__submit-button-inactive`}
+            ${isLoading ? `form__submit-button-inactive` : ``}
+            `}
         >
           Entrar
         </button>
